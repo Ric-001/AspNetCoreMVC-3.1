aa18032b532fe70c25118a325ef6bf10236b352e
Ricardo Alcaide Salas <ricalcaide@yahoo.es>
2020-10-27 19:06:46 +0100

    Added MVC to application

--- RicBookStore/RicBookStore/Startup.cs
+++ RicBookStore/RicBookStore/Startup.cs
@@ -16,6 +16,7 @@ namespace RicBookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,13 +27,53 @@ namespace RicBookStore
                 app.UseDeveloperExceptionPage();
             }
 
+
+            //app.Use(async (context,next) =>
+            //{
+            //    await context.Response.WriteAsync($"Hello from my first middleware{Environment.NewLine}");
+            //    await next();
+            //    await context.Response.WriteAsync($"Hello from my first middleware response{Environment.NewLine}");
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync($"Hello from my second middleware{Environment.NewLine}");
+            //    await next();
+            //    await context.Response.WriteAsync($"Hello from my second middleware response{Environment.NewLine}");
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync($"Hello from my third middleware{Environment.NewLine}");
+            //    await next();
+            //});
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                //endpoints.Map("/", async context =>
+                //{
+                //    if (env.IsDevelopment())
+                //        await context.Response.WriteAsync($"Hello Ric World!{Environment.NewLine}The enviroment is: Dev");
+                //    else if (env.IsProduction())
+                //        await context.Response.WriteAsync($"Hello Ric World!{Environment.NewLine}The enviroment is:Prod");
+                //    else if (env.IsStaging())
+                //        await context.Response.WriteAsync($"Hello Ric World!{Environment.NewLine}The enviroment is:Stag");
+                //    else if (env.IsEnvironment("Develop"))
+                //        await context.Response.WriteAsync($"Hello Ric World!{Environment.NewLine}The enviroment is:custom name Develop");
+
+
+                //});
+                
+                endpoints.MapDefaultControllerRoute();
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.Map("/Ric", async context =>
                 {
-                    await context.Response.WriteAsync("Hello Ric World!");
+                    await context.Response.WriteAsync($"Hello hello Ric World!{Environment.NewLine}");
                 });
             });
         }
