6acb532cb0cbc9f84226eb5b8355587aa5cd76b8
Ricardo Alcaide Salas <ricalcaide@yahoo.es>
2020-11-04 19:53:04 +0100

    Añadido libman gestor de bibliotercas

--- RicBookStore/RicBookStore/Startup.cs
+++ RicBookStore/RicBookStore/Startup.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace RicBookStore
@@ -29,6 +32,11 @@ namespace RicBookStore
 
             app.UseStaticFiles();
 
+            //app.UseStaticFiles(new StaticFileOptions()
+            //{
+            //    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "MyStaticFiles")),
+            //    RequestPath = "/MyStaticFiles"
+            //});
             //app.Use(async (context,next) =>
             //{
             //    await context.Response.WriteAsync($"Hello from my first middleware{Environment.NewLine}");
